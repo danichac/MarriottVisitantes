dc2789cbde0909976add30f1c226030146fefc75
danichac <danichac88@gmail.com>
2022-05-13 18:01:38 -0600

    Nullable fields

--- MarriottVisitantes.Dominio/Entidades/Visita.cs
+++ MarriottVisitantes.Dominio/Entidades/Visita.cs
@@ -38,7 +38,7 @@ namespace MarriottVisitantes.Dominio.Entidades
         public TipoVisita TipoVisita { get; set; }
 
         [MaxLength(8)]
-        public string Temperatura { get; set; }
+        public string? Temperatura { get; set; }
 
         [Required]
         public DateTime HoraEntrada { get; set; }
--- MarriottVisitantes.Repositorio/Migrations/20220513235950_NullableFields.Designer.cs
+++ MarriottVisitantes.Repositorio/Migrations/20220513235950_NullableFields.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MarriottVisitantes.Repositorio;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MarriottVisitantes.Repositorio.Migrations
 {
     [DbContext(typeof(MarriottVisitantesDbContext))]
-    partial class MarriottVisitantesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220513235950_NullableFields")]
+    partial class NullableFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
